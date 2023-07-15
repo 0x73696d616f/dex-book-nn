7805c21ff225717f4327e5bcf2967a484f937859
0x73696d616f <116109421+0x73696d616f@users.noreply.github.com>
2023-07-15 15:53:04 +0100

    feat: split validation and testing sets

--- nn.py
+++ nn.py
@@ -19,6 +19,7 @@ from sklearn.preprocessing import MinMaxScaler
 import json
 import os
 from datetime import date
+from sklearn.model_selection import train_test_split
 
 
 def create_lookback(dataset, look_back=1):
@@ -39,89 +40,80 @@ def train_model(X_train, Y_train, X_test, Y_test):
     
     # compile and fit the model
     model.compile(loss='mean_squared_error', optimizer='adam')
-    model.fit(X_train, Y_train, epochs = 100, batch_size = 30, shuffle = False, 
+    model.fit(X_train, Y_train, epochs = 100, batch_size = 16, shuffle = False, 
                     validation_data=(X_test, Y_test), verbose=0,
                     callbacks = [EarlyStopping(monitor='val_loss',min_delta=5e-5,patience=20,verbose=0)])
     return model
 
 
-# This function uses trained model and test dataset to calculate RMSE
-def get_rmse(model, X_test, Y_test, scaler, look_back = 1):    
+# This function uses trained model and validation dataset to calculate RMSE
+def get_rmse(model, X_val, Y_val, scaler, look_back = 1):    
     # get predictions and then make some transformations to be able to calculate RMSE properly in USD
-    prediction = model.predict(X_test)
+    prediction = model.predict(X_val)
     prediction_inverse = scaler.inverse_transform(prediction.reshape(-1, 1))
-    Y_test_inverse = scaler.inverse_transform(Y_test.reshape(-1, 1))
+    Y_val_inverse = scaler.inverse_transform(Y_val.reshape(-1, 1))
     prediction2_inverse = np.array(prediction_inverse[:,0][0:])
-    Y_test2_inverse = np.array(Y_test_inverse[:,0])
+    Y_val2_inverse = np.array(Y_val_inverse[:,0])
     
     #calculate RMSE
-    RMSE = sqrt(mean_squared_error(Y_test2_inverse, prediction2_inverse))
+    RMSE = sqrt(mean_squared_error(Y_val2_inverse, prediction2_inverse))
     return RMSE, prediction2_inverse
 
+
+dataset_size = 0.3
+test_size = 0.1
+val_size = 0.2
 
 data = pd.read_csv('bitcoin_price.csv')
 data['date'] = pd.to_datetime(data['Timestamp'],unit='s').dt.date
 group = data.groupby('date')
 Daily_Price = group['Weighted_Price'].mean()
+Daily_Price = Daily_Price[-int(dataset_size*len(Daily_Price)):]
 
-print(Daily_Price)
+# Split the data into training and testing datasets
+df_train, df_test = train_test_split(Daily_Price, test_size=test_size, shuffle=False)
 
-d0 = date(2016, 1, 1)
-d1 = date(2017, 10, 15)
-delta = d1 - d0
-days_look = delta.days + 1
-print("days_look: ", days_look)
+# Split the training dataset into training and validation datasets
+df_train, df_val = train_test_split(df_train, test_size=val_size, shuffle=False)
 
-d0 = date(2017, 8, 21)
-d1 = date(2017, 10, 20)
-delta = d1 - d0
-days_from_train = delta.days + 1
-print("days_from_train: ", days_from_train)
-
-d0 = date(2017, 10, 15)
-d1 = date(2017, 10, 20)
-delta = d1 - d0
-days_from_end = delta.days + 1
-print("days_from_end: ", days_from_end)
-
-df_train= Daily_Price[len(Daily_Price)-days_look-days_from_end:len(Daily_Price)-days_from_train]
-df_test= Daily_Price[len(Daily_Price)-days_from_train:]
-
-working_data = [df_train, df_test]
+working_data = [df_train, df_val, df_test]
 working_data = pd.concat(working_data)
 
 working_data = working_data.reset_index()
 working_data['date'] = pd.to_datetime(working_data['date'])
 working_data = working_data.set_index('date')
 
-df_train = working_data[:-60]
-df_test = working_data[-60:]
-
 training_set = df_train.values
 training_set = np.reshape(training_set, (len(training_set), 1))
+validation_set = df_val.values
+validation_set = np.reshape(validation_set, (len(validation_set), 1))
 test_set = df_test.values
 test_set = np.reshape(test_set, (len(test_set), 1))
 
 #scale datasets
 scaler = MinMaxScaler()
 training_set = scaler.fit_transform(training_set)
+validation_set = scaler.fit_transform(validation_set)
 test_set = scaler.fit_transform(test_set)
 
 # create datasets which are suitable for time series forecasting
-look_back = 1
+look_back = 10
 X_train, Y_train = create_lookback(training_set, look_back)
+X_val, Y_val = create_lookback(validation_set, look_back)
 X_test, Y_test = create_lookback(test_set, look_back)
+
 
 # reshape datasets so that they will be ok for the requirements of the LSTM model in Keras
 X_train = np.reshape(X_train, (len(X_train), X_train.shape[1], 1))
+X_val = np.reshape(X_val, (len(X_val), X_val.shape[1], 1))
 X_test = np.reshape(X_test, (len(X_test), X_test.shape[1], 1))
 
 # get predictions and then make some transformations to be able to calculate RMSE properly in USD
 Y_test_inverse = scaler.inverse_transform(Y_test.reshape(-1, 1))
 Y_test2_inverse = np.array(Y_test_inverse[:,0])
-Test_Dates = Daily_Price[len(Daily_Price)-days_from_train:].index
+Test_Dates = Daily_Price[int((1-test_size)*len(Daily_Price)):].index
 
-model = train_model(X_train, Y_train, X_test, Y_test)
+model = train_model(X_train, Y_train, X_val, Y_val)
 RMSE, predictions = get_rmse(model, X_test, Y_test, scaler, look_back)
 print('Test GRU model RMSE: %.3f' % RMSE)
 
